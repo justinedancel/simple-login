1780a71466b519d45ddf54d9873d67a1c187e250
Justine Dancel <justine.dancel204@gmail.com>
2022-11-19 23:22:03 -0600

    [Client] Login form validation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,14 @@ import './App.css';
 import Login from './components/Login';
 
 const App: React.FC = () => {
+
+  const handleLoginSubmit = (username: string, password: string) => {
+    console.log(`Form valid: ${username} ${password}`);
+  };
+
   return (
     <div className="App">
-      <Login />
+      <Login onLoginSubmit={handleLoginSubmit} />
     </div>
   );
 }
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,23 +1,84 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './Login.css';
 
-const Login: React.FC = () => {
+interface LoginProps {
+  onLoginSubmit: (username: string, password: string) => void;
+};
+
+const Login: React.FC<LoginProps> = props => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<{ username: string, password: string }>({
+    username: "Username is required.",
+    password: "Password is required.",
+  });
+
+  const onUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newUsername = e.target.value;
+
+    if (!newUsername) {
+      setErrors(oldErrors => ({...oldErrors, username: "Username is required."}));
+    } else if (newUsername.length < 6) {
+      setErrors(oldErrors => ({...oldErrors, username: "Username must be at least 6 characters."}));
+    } else {
+      setErrors(oldErrors => ({...oldErrors, username: ""}));
+    }
+
+    setUsername(newUsername);
+  };
+
+  const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newPassword = e.target.value;
+
+    if (!newPassword) {
+      setErrors(oldErrors => ({...oldErrors, password: "Password is required."}));
+    } else if (newPassword.length < 8) {
+      setErrors(oldErrors => ({...oldErrors, password: "Password must be at least 8 characters."}));
+    } else {
+      setErrors(oldErrors => ({...oldErrors, password: ""}));
+    }
+
+    setPassword(newPassword);
+  };
+
+  const loginSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (errors.username === "" && errors.password === "") {
+      props.onLoginSubmit(username, password)
+    } else {
+      console.log("Form is invalid.", errors)
+    }
+  };
+
   return (
     <div className="Login">
-      <form>
+      <form onSubmit={loginSubmitHandler}>
         <h2 className="logo">J</h2>
         <h1 className="header">Login</h1>
         <div className="input-container">
           <label htmlFor="username">
             Username
           </label>
-          <input type="text" name="username" placeholder="Type your username." />
+          <input
+            type="text"
+            name="username"
+            placeholder="Type your username."
+            onChange={onUsernameChange}
+          />
+          <span className="error-message">{errors.username}</span>
         </div>
         <div className="input-container">
           <label htmlFor="password">
             Password
           </label>
-          <input type="password" name="password" placeholder="Type your password." />
+          <input
+            type="password"
+            name="password"
+            placeholder="Type your password."
+            onChange={onPasswordChange}
+          />
+          <span className="error-message">{errors.password}</span>
         </div>
         <button className="submit" type="submit">Login</button>
       </form>
