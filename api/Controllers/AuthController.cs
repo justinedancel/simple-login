86a0861034990f26f427b718bc8eb4e171dd2cb5
Justine Dancel <justine.dancel204@gmail.com>
2022-11-23 21:00:23 -0600

    [API] Web Api for Login

---
+++ api/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using api.Models;
+using api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<User> _logger;
+        private readonly IAuthService _authService;
+
+        public AuthController(ILogger<User> logger, IAuthService authService)
+        {
+            _logger = logger;
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(User request)
+        {
+            string message;
+
+            try
+            {
+                message = _authService.LoginUser(request);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, request);
+
+                return BadRequest(ex.Message);
+            }
+            
+            return message;
+        }
+    }
+}
---
+++ api/Services/AuthService.cs
@@ -0,0 +1,35 @@
+﻿using api.Models;
+
+namespace api.Services
+{
+    public interface IAuthService
+    {
+        public string LoginUser(User user);
+    }
+
+    public class AuthService: IAuthService
+    {
+        private static readonly List<User> users = new List<User>()
+        {
+            { new User(){ Username = "testing", Password = "testing" } },
+            { new User(){ Username = "justine", Password = "dancel" } }
+        };
+
+        public string LoginUser(User user)
+        {
+            if (String.IsNullOrEmpty(user.Username) || String.IsNullOrEmpty(user.Password))
+            {
+                throw new Exception("Username & Password are both required.");
+            }
+
+            User dbUser = users.Find(x => x.Username == user.Username && x.Password == user.Password);
+
+            if (dbUser == null)
+            {
+                throw new Exception("User could not be found.");
+            }
+
+           return "Logged in";
+        }
+    }
+}
