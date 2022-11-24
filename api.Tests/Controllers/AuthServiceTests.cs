b41b369e9543a01c5bc05989f57da8ca0fa17aa3
Justine Dancel <justine.dancel204@gmail.com>
2022-11-24 15:49:49 -0600

    [API] Web api unit testing

---
+++ api.Tests/Controllers/AuthServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using api.Models;
+using api.Services;
+using Microsoft.Extensions.Logging;
+using FakeItEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.Controllers;
+
+namespace api.Tests.Controllers
+{
+    public class AuthServiceTests
+    {
+        [Fact]
+        public void AuthService_Login_Return_Exception_NotFound()
+        {
+            // Arrange
+            var user = new User() { Username = "Justine", Password = "Dancel" };
+
+            var authService = new AuthService();
+
+            // Act
+            var exception = Assert.Throws<Exception>(() => authService.LoginUser(user));
+
+            // Assert
+            Assert.Equal("User could not be found.", exception.Message);
+        }
+
+        [Fact]
+        public void AuthService_Login_Return_Exception_Invalid()
+        {
+            // Arrange
+            var user = new User() { Username = "", Password = "" };
+
+            var authService = new AuthService();
+
+            // Act
+            var exception = Assert.Throws<Exception>(() => authService.LoginUser(user));
+
+            // Assert
+            Assert.Equal("Username & Password are both required.", exception.Message);
+        }
+
+        [Fact]
+        public void AuthService_Login_Return_Valid()
+        {
+            // Arrange
+            var user = new User() { Username = "justine", Password = "dancel" };
+
+            var authService = new AuthService();
+
+            // Act
+            var result = authService.LoginUser(user);
+
+            // Assert
+            Assert.Equal("Logged in", result);
+        }
+    }
+}
